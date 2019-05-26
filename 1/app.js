93fc686a7e97110a364c1c1936ed35e096145a54
msagres <48651062+msagres@users.noreply.github.com>
2019-05-26 20:44:27 +0200

    Add files via upload

---
+++ 1/app.js
@@ -0,0 +1,69 @@
+// ENVIRONMENT
+const config = {
+    // window
+    width: 500,
+    height: 300,
+    type: Phaser.AUTO,
+    // action
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    },
+    // window physics
+    physics: {
+        default: 'arcade',
+        arcade: {
+            // verticaly physics
+            gravity: {
+                y: 450
+            }
+        }
+    }
+}
+
+
+// phaser framework
+var game = new Phaser.Game(config)
+// player image
+let dude
+let cursors
+
+
+function preload() {
+    // image source
+    this.load.image('dude', 'image/dude.png')
+}
+
+
+function create() {
+    // image player
+    dude = this.physics.add.image(100, 100, 'dude')
+    // allows sprites to collide with world bounds
+    dude.body.collideWorldBounds = true;
+    // key action 
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+    dude.setVelocityX(0)
+
+    // up key action
+    if (cursors.space.isDown) {
+        dude.setVelocity(0, -300)
+    }
+    /*
+    else if(cursors.up.isUp && dude.y >=300){
+        dude.setVelocity(0, 0)
+    }*/
+    // right key action
+    if (cursors.right.isDown) {
+        dude.setVelocity(300, 0)
+
+    }
+    // left key action
+    if (cursors.left.isDown) {
+        dude.setVelocity(-300, 0)
+    }
+
+}
